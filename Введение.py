cbbd1c0571a40f1e41bc7260a7be8f7fe2dde51e
83metra <jumo_004@mail.ru>
2024-06-15 13:02:01 +0200

    Add files via upload

---
+++ Введение.py
@@ -0,0 +1,29 @@
+# Длина слова
+a = 'Полярная лисица'
+print(len(a))
+
+# Сумма и разности
+first = 18
+second = 12
+summa = first + second
+diff = first - second
+print(f'Сумма = {summa}, разность = {diff}')
+
+# Среднее арифметическое
+first = 25
+second = 36
+third = 20
+mean = (first + second + third)/3
+print(f'Среднее арифметическое чисел {first}, {second}, {third} = {mean}')
+
+# Простые строчки
+first_string = ('Понедельник')
+second_string = ('Вторник')
+print(first_string + ', ' + second_string)
+
+# Сложная формула
+a = 2
+b = 3
+c = 7
+f = (a * b) + (a * c)
+print(f**3/2)
